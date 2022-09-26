14b49f723ea8fbf6d2559be6d3b9022f31d09da2
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-09-26 12:14:10 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+//Java Program to Add two Numbers
+
+class Add
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a= 10;
+b=20;
+c=a+b;
+System.out.println("Addition of two number is : " + c);
+}
+}
